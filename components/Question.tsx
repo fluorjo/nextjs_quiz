2c316f71374e735da26109869e4956658f4c503d
fluorjo <fluorjo@naver.com>
2023-12-30 13:19:01 +0900

    question component

--- components/Question.tsx
+++ components/Question.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
-const Question = () => {
-return (
-<div>Question</div>
-)
+import { IQuestion } from '@/utils/types';
+import React, { useState, useTransition } from 'react'
+interface Props {
+    question: IQuestion;
+    withId: boolean;
+    checkEnabled: boolean;
+    getAnswer?: (answer: boolean) => void;
+  }
+  
+  const Question = ({ question, withId, checkEnabled, getAnswer }: Props) => {
+    const [answer, setAnswer] = useState(0);
+    const [isAnswered, setIsAnswered] = useState(false);
+    const [isTrue, setIsTrue] = useState(false);
+  
+    const transitions = useTransition(isAnswered, {
+      from: { opacity: 0, height: 0 },
+      enter: { opacity: 1, height: 40 },
+      leave: { opacity: 0, height: 0 },
+      config: {
+        duration: 200,
+      },
+    });
 }
-
 export default Question
