3d4900c4b6bdb7b10d68babe52c92139310d1454
fluorjo <fluorjo@naver.com>
2023-12-31 22:05:57 +0900

    quiz page questions part

--- components/QuizForm.tsx
+++ components/QuizForm.tsx
@@ -77,6 +77,43 @@ const QuizForm = () => {
           </button>
         </div>
       )}
+
+      {quizStarted && !quizEnded && (
+        <div>
+          <Question
+            question={questions[currentQuestion]}
+            getAnswer={(answer) => {
+              setCurrentAnswer(answer);
+              setAnswered(true);
+            }}
+            withId={false}
+            checkEnabled={false}
+          />
+          <div>
+            <div>
+              문제 {currentQuestion + 1} /{" "}
+              {questions.length}
+            </div>
+
+            {answered && (
+              <button
+                className="my-4 px-8 py-2 text-white float-right rounded-lg shadow bg-blue-600 hover:bg-blue-500"
+                onClick={() =>
+                  currentQuestion >=
+                  questions.length - 1
+                    ? quizEnd()
+                    : nextQuestion()
+                }
+              >
+                {currentQuestion >=
+                questions.length - 1
+                  ? "완료"
+                  : "다음"}
+              </button>
+            )}
+          </div>
+        </div>
+      )}
     </div>
   );
 };
